f1e59310521ff58b6374005ed8a1ee8f15775087
Aleix Batlle <aleixbatlle@gmail.com>
2023-08-02 14:39:47 +0200

    .

--- Curso-React/wishlist/index.jsx
+++ Curso-React/wishlist/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-import  ReactDOM from "react-dom"; 
+import ReactDOM from "react-dom"; 
 
 ReactDOM.render(
     <div>My wishlist from REACT</div>, document.getElementById('root')
